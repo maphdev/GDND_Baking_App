34c8bd8dc67b70161930944a04e249eca5571340
Manon Philippot <33070721+maphdev@users.noreply.github.com>
2018-07-19 23:40:54 +0200

    Espresso.

--- app/src/androidTest/java/com/example/manon/bakingapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/manon/bakingapp/ExampleInstrumentedTest.java
@@ -1,22 +1,17 @@
 package com.example.manon.bakingapp;
 
 
-import android.content.Intent;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import com.example.manon.bakingapp.Activity.ListIngredientsActivity;
 import com.example.manon.bakingapp.Activity.MainActivity;
-import com.example.manon.bakingapp.Widget.ListIngredientsWidgetProvider;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
